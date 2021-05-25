4ae046e818de9a62454675a7a5873d5b3b705436
github tanayu567 <tana-yu@tanakayuutanoMacBook-Air.local>
2021-05-26 08:13:41 +0900

    Fix top page

--- app/controllers/workers_controller.rb
+++
@@ -1,4 +0,0 @@
-class WorkersController < ApplicationController
-  def new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get  '/signup',  to: 'workers#new'
+  root 'top#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
