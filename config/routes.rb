5e5886211adfcaa5d8b2d81f113805dc1b79efbf
github tanayu567 <tana-yu@tanakayuutanoMacBook-Air.local>
2021-05-30 15:27:04 +0900

    Implementing gest login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, 
     controllers: { registrations: 'registrations' } 
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   root 'top#index'
+  
 
   resources :users, only: [:show]
   
