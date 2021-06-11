7c6541182b56434388a50df0f1fa7ac401af1c35
github tanayu567 <tana-yu@tanakayuutanoMacBook-Air.local>
2021-06-11 14:49:50 +0900

    add relationship column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_065506) do
+ActiveRecord::Schema.define(version: 2021_06_11_054524) do
 
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2021_06_09_065506) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["spot_id"], name: "index_likes_on_spot_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "relationships", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "spots", charset: "utf8mb4", force: :cascade do |t|
