4260900533d7ec363649dd06e87ed6069e0a15d4
github tanayu567 <tana-yu@tanakayuutanoMacBook-Air.local>
2021-06-15 10:52:01 +0900

    add factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do         
+      it '正常に登録できること' do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+        user.save
+      end
+
+    describe '登録に失敗する' do
+      it 'パスワードがない場合に登録失敗' do
+        user = FactoryBot.build(:user)
+        user.password = nil
+        expect(user).to be_invalid
+      end
+
+      it '名前がない場合に登録失敗' do
+        user = FactoryBot.build(:user)
+        user.name = nil
+        expect(user).to be_invalid
+      end
+
+      it 'パスワードがない場合に登録失敗' do
+        user = FactoryBot.build(:user)
+        user.password = nil
+        expect(user).to be_invalid
+      end
+    end
+
+  end
+  
 end
