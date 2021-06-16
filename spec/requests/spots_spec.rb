94f1f1d9a5927f21810a783b33c32a2d1eb05cdc
github tanayu567 <tana-yu@tanakayuutanoMacBook-Air.local>
2021-06-16 20:59:55 +0900

    検索結果にヘッダーを追加

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Spots", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "現場検索" do
+    it "正常に一覧を表示する" do
+      get search_spots_path
+      expect(response).to have_http_status(200)
+    end
+    
+    
   end
 end
