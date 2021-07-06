0d3e400dab8307c41d23aee56347d06a82101a8a
tanayu567 <tana-yu.567@outlook.jp>
2021-07-06 15:45:27 +0900

    unicorn.rb内容修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,14 +6,11 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
 #ポート番号を指定
 # listen 3000
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+#Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 #通常のログを記録するファイルを指定
