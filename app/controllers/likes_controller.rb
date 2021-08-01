a8c36ada8b51789a3593d0b084537dc23a8c8a14
tanayu567 <tana-yu.567@outlook.jp>
2021-08-01 21:44:39 +0900

    コードの微調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,12 @@ class LikesController < ApplicationController
   def create
     @spot = Spot.find(params[:spot_id])
     @like = current_user.likes.create(spot_id: params[:spot_id])
-    
     redirect_back(fallback_location: root_path)
   end
   def destroy
     @spot = Spot.find(params[:spot_id])
     @like = current_user.likes.find_by(spot_id: @spot.id)
     @like.destroy
-   
     redirect_back(fallback_location: root_path)
   end
 
