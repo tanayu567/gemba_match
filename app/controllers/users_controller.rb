56cf446592bfda09783d0b5c0a1e9960ac79ee19
tanayu567 <tana-yu.567@outlook.jp>
2021-08-08 21:04:47 +0900

    削除機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :admin_user, only: :destroy
   
   def show
     @user = User.find(params[:id]) 
     @spots = @user.spots
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_url
   end
 
   def search
@@ -23,5 +30,16 @@ class UsersController < ApplicationController
     # @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_054524) do
+ActiveRecord::Schema.define(version: 2021_08_08_043356) do
 
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_054524) do
     t.string "home"
     t.text "license"
     t.string "profile_image"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
