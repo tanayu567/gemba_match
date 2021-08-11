39654678bc8b5cf5cf2cb84bb39bf67839c50a1c
tanayu567 <tana-yu.567@outlook.jp>
2021-08-11 21:25:21 +0900

    プロフィール編集の修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,6 @@ import '../stylesheets/application';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 
-// import Vue from 'vue'
-// import Vuex from 'vuex'
-// import App from '../app.vue'
-// import store from './store.js'
-// import router from './router.js'
-
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
