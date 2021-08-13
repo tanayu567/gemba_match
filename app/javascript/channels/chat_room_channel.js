10c565cfc2d747b0a9aa4d04514c4f1bdbb52c2b
tanayu567 <tana-yu.567@outlook.jp>
2021-08-13 09:26:43 +0900

    chat_room_channel.jsをエラー前に戻した

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -10,20 +10,18 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   },
 
   received(data) {
-    const chatMessages = document.getElementById('chat-messages');
-    chatMessages.insertAdjacentHTML('beforeend', data['chat_message']);
+    return alert(data['chat_message']);
   },
 
-  speak: function(chat_message, chat_room_id) {
-    return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
+  speak: function(chat_message) {
+    return this.perform('speak', { chat_message: chat_message });
   }
 });
 
 if(/chat_rooms/.test(location.pathname)) {
   $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
     if (e.key === "Enter") {
-      const chat_room_id = $('textarea').data('chat_room_id')
-      appChatRoom.speak(e.target.value, chat_room_id);
+      appChatRoom.speak(e.target.value);
       e.target.value = '';
       e.preventDefault();
     }
