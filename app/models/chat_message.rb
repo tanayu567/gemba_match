76034541b7188a922e40c02d8a5f62b826a078eb
tanayu567 <tana-yu.567@outlook.jp>
2021-08-13 11:44:37 +0900

    メッセージページまでの作成

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -2,5 +2,5 @@ class ChatMessage < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
 
-  after_create_commit { ChatMessageBroadcastJob.perform_later self }
+  # after_create_commit { ChatMessageBroadcastJob.perform_later self }
 end
