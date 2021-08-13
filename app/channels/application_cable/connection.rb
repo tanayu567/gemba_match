9143555c1dd11b4b2d81f17b7a6a7f6af14e3bc1
tanayu567 <tana-yu.567@outlook.jp>
2021-08-13 12:46:29 +0900

    エラー発生時のコードに戻した

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,16 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
+    identified_by :current_user
 
-    # def connect
-    #   reject_unauthorized_connection unless find_verified_user
-    # end
+    def connect
+      reject_unauthorized_connection unless find_verified_user
+    end
 
-    # private
+    private
 
-    #   def find_verified_user
-    #     self.current_user = env['warden'].user
-    #   end
+      def find_verified_user
+        self.current_user = env['warden'].user
+      end
       
   end
 end
