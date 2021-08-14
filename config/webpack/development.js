849b3c8458236df62481cb4a3e08f327319f6200
tanayu567 <tana-yu.567@outlook.jp>
2021-08-14 20:46:43 +0900

    不要なファイル削除

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,10 +4,3 @@ const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
 
-// module.exports = Object.assign({}, environment.toWebpackConfig(), {
-//   resolve: {
-//     alias: {
-//       'vue$': 'vue/dist/vue.esm.js'
-//     }
-//   }
-// })
