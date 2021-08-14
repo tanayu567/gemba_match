6eda797525c084adc19d97edc68189f91847e8cb
tanayu567 <tana-yu.567@outlook.jp>
2021-08-14 17:04:42 +0900

    jqueryの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,8 +10,8 @@ const webpack = require('webpack')
 environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
-    // $: 'jquery/src/jquery',
-    // jQuery: 'jquery/src/jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
 ) 
