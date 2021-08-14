bd83c7312f6a3822fde0d0fb78318ad899276aaa
tanayu567 <tana-yu.567@outlook.jp>
2021-08-14 20:30:42 +0900

    enviroment.jsを修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,17 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+// const { VueLoaderPlugin } = require('vue-loader')
+// const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+// environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+// environment.loaders.prepend('vue', vue)
 module.exports = environment
 
 const webpack = require('webpack')
 environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
-    // $: 'jquery/src/jquery',
-    // jQuery: 'jquery/src/jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
-) 
+)  
