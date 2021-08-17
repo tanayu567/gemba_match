24085be04c6508543708a7545c88150a7bede9ef
tanayu567 <tana-yu.567@outlook.jp>
2021-08-17 16:16:52 +0900

    keypressに変更

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -20,8 +20,7 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
 });
 
 if(/chat_rooms/.test(location.pathname)) {
-  // $(document).on("keydown", ".chat-room__message-form_textarea", function(e) { 
-    window.addEventListener("keypress", function(e) {
+  $(document).on("keypress", ".chat-room__message-form_textarea", function(e) { 
     if (e.key === "Enter") {
       const chat_room_id = $('textarea').data('chat_room_id')
       appChatRoom.speak(e.target.value, chat_room_id);
