88cf3146ee41c73159547e92a3642763947e149e
tanayu567 <tana-yu.567@outlook.jp>
2021-08-17 17:05:25 +0900

    actioncableの設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://35.75.65.66/cable" 
+  config.action_cable.allowed_request_origins = ['http://35.75.65.66']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
