dcb3f3ed382709a8b0999a8b724d5ab27a4bb1fb
tanayu567 <tana-yu.567@outlook.jp>
2021-08-25 08:29:02 +0900

    いいね機能にAjaxを導入

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,14 @@ class LikesController < ApplicationController
   def create
     @spot = Spot.find(params[:spot_id])
     @like = current_user.likes.create(spot_id: params[:spot_id])
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
     @spot = Spot.find(params[:spot_id])
     @like = current_user.likes.find_by(spot_id: @spot.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   
